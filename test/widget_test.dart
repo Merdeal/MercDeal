a964a5ea62fc56000b0c24d0ee27a8ff15429cbd
Merdeal <casalini.carlo88@gmail.com>
2026-09-17 23:14:09 +0200

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,11 @@ import 'package:mercdeal/app/app.dart';
 void main() {
   testWidgets('MercDeal avvia la Home', (tester) async {
     await tester.pumpWidget(const MercDealApp());
+
+    // La splash resta visibile per 1200 ms prima di aprire la Home.
+    await tester.pump(const Duration(milliseconds: 1200));
+    await tester.pumpAndSettle();
+
     expect(find.text('MercDeal'), findsOneWidget);
     expect(find.text('Scopri ora'), findsOneWidget);
   });
